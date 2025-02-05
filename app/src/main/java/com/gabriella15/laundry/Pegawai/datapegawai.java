d7c7004d69b093eb685e4ea18f3d9c12f487be2c
gabriellagaby <imgurlsgaby@gmail.com>
2025-02-05 10:46:03 +0700

    menambahkan data

---
+++ app/src/main/java/com/gabriella15/laundry/Pegawai/datapegawai.java
@@ -0,0 +1,6 @@
+package com.gabriella15.laundry.Pegawai;
+
+import android.app.Activity;
+
+public class datapegawai extends Activity {
+}
--- app/src/main/java/pelanggan/Datapelanggan.kt
+++ app/src/main/java/pelanggan/Datapelanggan.kt
@@ -1,4 +1,4 @@
-package com.gabriella15.laundry.laundry
+package pelanggan
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.gabriella15.laundry.R
 
-class tambahpelanggan : AppCompatActivity() {
+class datapelanggan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tambahpelanggan)
+        setContentView(R.layout.activity_datapelanggan)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
